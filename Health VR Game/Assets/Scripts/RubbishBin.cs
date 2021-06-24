de960aba7c05f2d346550241343e19b272bd302f
lilwindax <72056829+lilwindax@users.noreply.github.com>
2021-06-24 18:16:34 +1200

    Update

--- Health VR Game/Assets/Scripts/RubbishBin.cs
+++ Health VR Game/Assets/Scripts/RubbishBin.cs
@@ -6,7 +6,47 @@ public class RubbishBin : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Rubbish")
+        if (collision.gameObject.tag != "Rubbish" )
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag != "Rubbish1")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag != "r1")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag != "r2")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag != "r3")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag != "r4")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag != "r5")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag != "r6")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag != "r7")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag != "r8")
+        {
+            Destroy(collision.gameObject);
+        }
+        if (collision.gameObject.tag != "r9")
         {
             Destroy(collision.gameObject);
         }
